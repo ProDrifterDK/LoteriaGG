15bc32aa0798bbc505cd199f6e2a7d0719ce0d93
Daniel Barria <daniel.b.a@hotmail.es>
2017-07-22 21:03:09 -0400

    alan ctm

--- LoteriaGG/LoteriaGG/Areas/Steam/Controllers/WheelController.cs
+++ LoteriaGG/LoteriaGG/Areas/Steam/Controllers/WheelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datos.SqlData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,40 @@ namespace LoteriaGG.Areas.Steam.Controllers
         // GET: Steam/Weel
         public ActionResult Index()
         {
+            if (Session["LogedIn"] == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "Steam" });
+            }
+            var user = Session["User"];
+            using (var db = new LOTERIA_GGEntities())
+            {
+                var usu = db.TBL_USUARIO.FirstOrDefault(o => o.USU_ACCOUNT == user);
+                if (usu.USU_DAILY_REWARD == null || usu.USU_DAILY_REWARD.Value.Day != DateTime.Now.Day)
+                {
+                    ViewBag.Spin = 1;
+                }
+            }
             return View();
+        }
+
+        public ActionResult RecibirPremio(string val = "")
+        {
+            if (Session["LogedIn"] == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "Steam" });
+            }
+
+            var user = Session["User"];
+            using (var db = new LOTERIA_GGEntities())
+            {
+                var usu = db.TBL_USUARIO.FirstOrDefault(o => o.USU_ACCOUNT == user);
+                if (usu.USU_DAILY_REWARD != null || usu.USU_DAILY_REWARD.Value.Day == DateTime.Now.Day)
+                {
+                    return RedirectToAction("Index", new { });
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
