c0422049ef7f1b0e7a1fe3cd1b74beca6d856ba9
Alan Gárate <CDL.ProDrifter.DK@hotmail.com>
2017-11-27 15:08:52 -0300

    Mini bosquejo del Contactanos. Imagen del logo al medio perdida (dont know why :s)

---
+++ LoteriaGG/LoteriaGGNew/Areas/LoL/Controllers/ContactanosController.cs
@@ -0,0 +1,22 @@
+﻿using LoteriaGG.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoteriaGGNew.Areas.LoL.Controllers
+{
+    public class ContactanosController : BaseController
+    {
+        // GET: LoL/ObtenerGGCoin
+        public ActionResult Index()
+        {
+            if (UsuarioLogged == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
