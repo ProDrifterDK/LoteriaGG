53e0f2208f4d56c59ca7831e4344d47c95b42d72
Alan Gárate <CDL.ProDrifter.DK@hotmail.com>
2018-11-06 00:07:40 -0300

    Cambios varios a la interfaz en Shared/_Layout, Choose y en LoL -> Home/Index.

---
+++ LoteriaGG/LoteriaGGNew/Areas/LoL/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using LoteriaGG.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoteriaGGNew.Areas.LoL.Controllers
+{
+    public class AboutController : BaseController
+    {
+        // GET: LoL/About
+        public ActionResult Index()
+        {
+            if (UsuarioLogged == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
